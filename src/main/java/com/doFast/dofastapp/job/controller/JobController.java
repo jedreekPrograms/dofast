5b18fd11cd4df586656394779595eb538e57ea4b
Jedrek <jedreek323@gmail.com>
2026-02-16 23:33:16 +0100

    zlecenia

---
+++ src/main/java/com/doFast/dofastapp/job/controller/JobController.java
@@ -0,0 +1,35 @@
+package com.doFast.dofastapp.job.controller;
+
+import com.doFast.dofastapp.job.dto.JobRequest;
+import com.doFast.dofastapp.job.dto.JobResponse;
+import com.doFast.dofastapp.job.service.JobService;
+import com.doFast.dofastapp.user.entity.User;
+import jakarta.validation.Valid;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jobs")
+public class JobController {
+
+    private final JobService jobService;
+
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    @PostMapping
+    public JobResponse createJob(@RequestBody @Valid JobRequest request) {
+
+        Authentication authentication =
+                SecurityContextHolder.getContext().getAuthentication();
+
+        User user = (User) authentication.getPrincipal();
+
+        return jobService.createJob(request, user);
+    }
+}
