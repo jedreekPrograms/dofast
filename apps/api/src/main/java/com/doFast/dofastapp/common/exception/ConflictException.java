886451c084a0b586cb74a101a31e6f172729ec21
Jędrzej Stefanowski <jedreek323@gmail.com>
2026-08-25 14:54:03 +0200

    feat(api): add conflict domain exception

---
+++ apps/api/src/main/java/com/doFast/dofastapp/common/exception/ConflictException.java
@@ -0,0 +1,8 @@
+package com.doFast.dofastapp.common.exception;
+
+public class ConflictException extends RuntimeException {
+
+    public ConflictException(String message) {
+        super(message);
+    }
+}
