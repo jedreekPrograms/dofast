b0fd80879a9d7529b1ac5e211f2228879beae8c1
Jędrzej Stefanowski <jedreek323@gmail.com>
2026-08-25 17:06:38 +0200

    Carlisle: lock escrow during state transitions

--- apps/api/src/main/java/com/doFast/dofastapp/payment/repository/TransactionRepository.java
+++ apps/api/src/main/java/com/doFast/dofastapp/payment/repository/TransactionRepository.java
@@ -2,11 +2,19 @@ package com.doFast.dofastapp.payment.repository;
 
 import com.doFast.dofastapp.job.entity.Job;
 import com.doFast.dofastapp.payment.entity.Transaction;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
     Optional<Transaction> findByJob(Job job);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select t from Transaction t where t.job = :job")
+    Optional<Transaction> findByJobForUpdate(@Param("job") Job job);
 }
