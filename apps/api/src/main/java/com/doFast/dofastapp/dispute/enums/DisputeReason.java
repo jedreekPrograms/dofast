9342963b11b1f10edbac5a5f858be3be85253e97
Jędrzej Stefanowski <jedreek323@gmail.com>
2026-08-25 17:05:03 +0200

    Carlisle: add dispute reasons

---
+++ apps/api/src/main/java/com/doFast/dofastapp/dispute/enums/DisputeReason.java
@@ -0,0 +1,10 @@
+package com.doFast.dofastapp.dispute.enums;
+
+public enum DisputeReason {
+    NOT_COMPLETED,
+    QUALITY_ISSUE,
+    NO_SHOW,
+    PAYMENT_ISSUE,
+    SAFETY_CONCERN,
+    OTHER
+}
