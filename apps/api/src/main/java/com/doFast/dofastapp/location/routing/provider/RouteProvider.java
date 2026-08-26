2560621bd3bfcf67688d84bae5517c9789b79495
Jędrzej Stefanowski <jedreek323@gmail.com>
2026-08-26 18:48:17 +0200

    routing: support ordered intermediate waypoints

--- apps/api/src/main/java/com/doFast/dofastapp/location/routing/provider/RouteProvider.java
+++ apps/api/src/main/java/com/doFast/dofastapp/location/routing/provider/RouteProvider.java
@@ -1,10 +1,25 @@
 package com.doFast.dofastapp.location.routing.provider;
 
+import java.util.List;
+
 public interface RouteProvider {
 
     default RouteProviderResult estimate(RouteCoordinate origin, RouteCoordinate destination) {
-        return estimate(origin, destination, RouteTravelMode.DRIVE);
+        return estimate(origin, List.of(), destination, RouteTravelMode.DRIVE);
     }
 
-    RouteProviderResult estimate(RouteCoordinate origin, RouteCoordinate destination, RouteTravelMode travelMode);
+    default RouteProviderResult estimate(
+            RouteCoordinate origin,
+            RouteCoordinate destination,
+            RouteTravelMode travelMode
+    ) {
+        return estimate(origin, List.of(), destination, travelMode);
+    }
+
+    RouteProviderResult estimate(
+            RouteCoordinate origin,
+            List<RouteCoordinate> intermediates,
+            RouteCoordinate destination,
+            RouteTravelMode travelMode
+    );
 }
