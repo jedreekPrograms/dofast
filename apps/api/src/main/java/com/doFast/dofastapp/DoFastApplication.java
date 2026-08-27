d70828046771ab7fdfd0b022adac89f814e5c758
Jędrzej Stefanowski <jedreek323@gmail.com>
2026-08-27 04:10:15 +0200

    jobs: enable scheduled outbox worker

--- apps/api/src/main/java/com/doFast/dofastapp/DoFastApplication.java
+++ apps/api/src/main/java/com/doFast/dofastapp/DoFastApplication.java
@@ -2,7 +2,9 @@ package com.doFast.dofastapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class DoFastApplication {
 
