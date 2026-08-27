99567ce8a1c7111f57e946d6de219e9e086b035d
Jędrzej Stefanowski <jedreek323@gmail.com>
2026-08-27 02:07:40 +0200

    notifications: make preference replacement deterministic

--- apps/api/src/main/java/com/doFast/dofastapp/notification/repository/NotificationPreferenceRepository.java
+++ apps/api/src/main/java/com/doFast/dofastapp/notification/repository/NotificationPreferenceRepository.java
@@ -4,11 +4,16 @@ import com.doFast.dofastapp.notification.entity.NotificationPreference;
 import com.doFast.dofastapp.notification.enums.NotificationType;
 import com.doFast.dofastapp.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface NotificationPreferenceRepository extends JpaRepository<NotificationPreference, Long> {
     boolean existsByUserAndNotificationType(User user, NotificationType notificationType);
     List<NotificationPreference> findAllByUser(User user);
-    void deleteAllByUser(User user);
+
+    @Modifying
+    @Query("delete from NotificationPreference preference where preference.user = :user")
+    int deleteAllForUser(User user);
 }
