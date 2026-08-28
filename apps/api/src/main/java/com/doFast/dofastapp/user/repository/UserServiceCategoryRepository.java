19cf1a6b7d01cf768e399853d292c4cb2f5922f6
Jędrzej Stefanowski <jedreek323@gmail.com>
2026-08-28 10:23:35 +0200

    users: make specialization lookup explicit

--- apps/api/src/main/java/com/doFast/dofastapp/user/repository/UserServiceCategoryRepository.java
+++ apps/api/src/main/java/com/doFast/dofastapp/user/repository/UserServiceCategoryRepository.java
@@ -3,11 +3,19 @@ package com.doFast.dofastapp.user.repository;
 import com.doFast.dofastapp.user.entity.UserServiceCategory;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface UserServiceCategoryRepository extends JpaRepository<UserServiceCategory, Long> {
 
     @EntityGraph(attributePaths = {"category", "category.parent"})
-    List<UserServiceCategory> findAllByUser_IdOrderByCategory_SortOrderAscCategory_NameAsc(Long userId);
+    @Query("""
+            select relation
+            from UserServiceCategory relation
+            where relation.user.id = :userId
+            order by relation.category.sortOrder asc, relation.category.name asc
+            """)
+    List<UserServiceCategory> findForUser(@Param("userId") Long userId);
 }
