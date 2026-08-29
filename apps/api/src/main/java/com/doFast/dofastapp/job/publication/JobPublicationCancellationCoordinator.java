2e6932459d509c0842538fbd40634631375deb77
Jędrzej Stefanowski <jedreek323@gmail.com>
2026-08-29 18:19:27 +0200

    payments: cancel abandoned publication intents

---
+++ apps/api/src/main/java/com/doFast/dofastapp/job/publication/JobPublicationCancellationCoordinator.java
@@ -0,0 +1,26 @@
+package com.doFast.dofastapp.job.publication;
+
+import com.doFast.dofastapp.job.publication.dto.JobPublicationResponse;
+import com.doFast.dofastapp.user.entity.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JobPublicationCancellationCoordinator {
+
+    private final JobPublicationService publicationService;
+    private final JobPublicationPaymentIntentCleanupService paymentIntentCleanupService;
+
+    public JobPublicationCancellationCoordinator(
+            JobPublicationService publicationService,
+            JobPublicationPaymentIntentCleanupService paymentIntentCleanupService
+    ) {
+        this.publicationService = publicationService;
+        this.paymentIntentCleanupService = paymentIntentCleanupService;
+    }
+
+    public JobPublicationResponse cancel(Long publicationId, User currentUser) {
+        JobPublicationResponse response = publicationService.cancel(publicationId, currentUser);
+        paymentIntentCleanupService.cancelAttachedIntentBestEffort(publicationId);
+        return response;
+    }
+}
