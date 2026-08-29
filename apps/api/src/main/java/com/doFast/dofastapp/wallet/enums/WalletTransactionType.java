df4b377adb7124cf081d5632b07eae735d5a6bac
Jędrzej Stefanowski <jedreek323@gmail.com>
2026-08-29 08:21:28 +0200

    wallet: distinguish publication Stripe funding

--- apps/api/src/main/java/com/doFast/dofastapp/wallet/enums/WalletTransactionType.java
+++ apps/api/src/main/java/com/doFast/dofastapp/wallet/enums/WalletTransactionType.java
@@ -2,6 +2,7 @@ package com.doFast.dofastapp.wallet.enums;
 
 public enum WalletTransactionType {
     TOP_UP,
+    JOB_PUBLICATION_FUNDING,
     ESCROW_LOCK,
     ESCROW_ADJUSTMENT_LOCK,
     ESCROW_ADJUSTMENT_REFUND,
