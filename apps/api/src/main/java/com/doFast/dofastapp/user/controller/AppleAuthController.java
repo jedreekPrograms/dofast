f6448116b7c5f6ef2701ef49103c43e918624586
Jędrzej Stefanowski <jedreek323@gmail.com>
2026-08-30 12:04:49 +0200

    auth: issue refresh cookies on Apple login

--- apps/api/src/main/java/com/doFast/dofastapp/user/controller/AppleAuthController.java
+++ apps/api/src/main/java/com/doFast/dofastapp/user/controller/AppleAuthController.java
@@ -5,8 +5,12 @@ import com.doFast.dofastapp.user.auth.apple.AppleLoginChallengeResponse;
 import com.doFast.dofastapp.user.auth.apple.AppleLoginChallengeService;
 import com.doFast.dofastapp.user.auth.apple.AppleLoginRequest;
 import com.doFast.dofastapp.user.auth.apple.AppleSignInService;
+import com.doFast.dofastapp.user.auth.session.AuthSessionCookieService;
+import com.doFast.dofastapp.user.auth.session.AuthSessionGrant;
+import com.doFast.dofastapp.user.auth.session.AuthSessionService;
 import com.doFast.dofastapp.user.dto.AuthResponse;
 import com.doFast.dofastapp.user.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,15 +24,21 @@ public class AppleAuthController {
     private final AppleLoginChallengeService challengeService;
     private final AppleSignInService appleSignInService;
     private final UserService userService;
+    private final AuthSessionService sessionService;
+    private final AuthSessionCookieService cookieService;
 
     public AppleAuthController(
             AppleLoginChallengeService challengeService,
             AppleSignInService appleSignInService,
-            UserService userService
+            UserService userService,
+            AuthSessionService sessionService,
+            AuthSessionCookieService cookieService
     ) {
         this.challengeService = challengeService;
         this.appleSignInService = appleSignInService;
         this.userService = userService;
+        this.sessionService = sessionService;
+        this.cookieService = cookieService;
     }
 
     @PostMapping("/challenge")
@@ -37,8 +47,13 @@ public class AppleAuthController {
     }
 
     @PostMapping
-    public AuthResponse login(@RequestBody @Valid AppleLoginRequest request) {
+    public AuthResponse login(
+            @RequestBody @Valid AppleLoginRequest request,
+            HttpServletResponse response
+    ) {
         AppleIdentity identity = appleSignInService.authenticate(request);
-        return userService.loginWithAppleIdentity(identity);
+        AuthSessionGrant grant = sessionService.issue(userService.loginWithAppleIdentity(identity));
+        cookieService.write(response, grant);
+        return grant.response();
     }
 }
